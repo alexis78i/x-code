59af222c55dec82cae942dddcaade25e80733dd5
Alex <alexis78i@yandex.ru>
2024-07-15 22:36:42 +0000

    autocomplete

--- app/Services/Public/PublicAutocompleteService.php
+++ app/Services/Public/PublicAutocompleteService.php
@@ -9,12 +9,12 @@ class PublicAutocompleteService
     public function search($request) : array
     {
         $query = $request->input('q', '');
-        $searchQuery = str_replace(' ', ' & ', $query);
+        $searchQuery = '%' . str_replace(' ', '%', $query) . '%';
 
         $results = DB::table('houses')
             ->join('streets', 'houses.street_id', '=', 'streets.id')
             ->select('houses.slug', 'streets.name as street_name', 'streets.postcode_id as postcode', 'houses.house_number')
-            ->whereRaw("address_search @@ to_tsquery('simple', ?)", [$searchQuery])
+            ->where('streets.name', 'ILIKE', $searchQuery) // Using ILIKE for case-insensitive partial matching
             ->limit(25)
             ->get();
 
