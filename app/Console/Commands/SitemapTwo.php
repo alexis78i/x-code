87b06aad9589f0b18bb47ee7a6ea186a8a632b7d
Alex <alexis78i@yandex.ru>
2024-07-15 23:46:57 +0000

    sitemap

---
+++ app/Console/Commands/SitemapTwo.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Apartments\Apartments;
+use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Storage;
+
+class SitemapTwo extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:sitemap-two';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $apartments = Apartments::limit(50000)->offset(50000)->get();
+        $sitemap = '<?xml version="1.0" encoding="UTF-8"?>';
+        $sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+
+        foreach ($apartments as $product) {
+            $sitemap .= '<url>
+                            <loc>https://procura.one/verdmat/'.$product->slug.'</loc>
+                            <lastmod>'.Carbon::now()->format('Y-m-d').'</lastmod>
+                            <changefreq>daily</changefreq>
+                            <priority>0.8</priority>
+                        </url>';
+        }
+
+        $sitemap .= '</urlset>';
+
+        Storage::disk('public')->put('sitemaps/procura-one-2.xml', $sitemap);
+    }
+}
