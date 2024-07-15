313dac973780c0f07f2021f806deb7ba95ce633f
Alex <alexis78i@yandex.ru>
2024-07-15 19:19:26 +0000

    add slug

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('frontend')->group(function () {
-    Route::get('/apartment', [App\Http\Controllers\Public\Apartments\ApartmentsController::class, 'view']);
+    Route::get('/apartment/{slug}', [App\Http\Controllers\Public\Apartments\ApartmentsController::class, 'view']);
 });
